669d152651d09c0141ebcf55067f476d00b5c434
John Garrett <jacgarrett18@gmail.com>
2020-10-15 13:38:51 -0400

    palendromic substring

--- longest-palindromic-substring/main.py
+++ longest-palindromic-substring/main.py
@@ -1,23 +1,21 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        def isPalendromic(s: str) -> bool:
-            length = len(s)
-            if length <= 1:
-                return True
-            elif length == 2:
-                return s[:1] == s[1:]
-            elif length % 2 == 0:
-                return False
-            # TODO: reverse second string
-            else:
-                middle = int((len(s)) / 2)
-                return s[:middle] == s[middle + 1:]
+def longestPalindrome(s: str) -> str:
+    isPalendromic = lambda s: s == s[::-1]
 
-        longestPal = ""
-        for i in range(len(s)):
-            pal = ""
-            for j in range(i, len(s)):
-                pal = pal + s[j]
-                if isPalendromic(pal) and len(pal) > len(longestPal):
-                    longestPal = pal
-            return longestPal
+    if isPalendromic(s):
+        return s
+
+    longestPal = s[0]
+    length = len(s)
+    for i in range(length):
+        pal = ""
+        for j in range(i, length):
+            pal += s[j]
+            if isPalendromic(pal) and len(pal) > len(longestPal):
+                longestPal = pal
+        return longestPal
+
+
+print(longestPalindrome("lipwawibllrziekxgwudqghfpvsafguorthpsdihcinuasyzmttzxdluhrnfdrawabwxdgpoqabfhutzowqfhkynrhobyuygesngyxpjyilqhwyeemklicinmatyishobtitukbkpqtxwioqnztlewilnewokfqkycfuvgqmogwuvkrxphyjvhbkhpcwywfnazsoulmgdoaxyngoynmfexdcpanoyidutpzcicibjnzmybvggqbpbejsvliocotewgrfcwyebisiywjsugjxxwupryxglvkgdugbejsibuscjofrvaeexqweieldfhriftlczbuzmuizjqzxovziflaigwxrxowmhdlvrbxzeaaqxmicvigolodopbukjvkzwvxexnnweodsoscnpmuwgjhmlurwdqbwrzavjjubsueahunqwemmewqnuhaeusbujjvazrwbqdwrulmhjgwumpncsosdoewnnxexvwzkvjkubpodologivcimxqaaezxbrvldhmwoxrxwgialfizvoxzqjziumzubzcltfirhfdleiewqxeeavrfojcsubisjebgudgkvlgxyrpuwxxjgusjwyisibeywcfrgwetocoilvsjebpbqggvbymznjbiciczptudiyonapcdxefmnyognyxaodgmluoszanfwywcphkbhvjyhpxrkvuwgomqgvufcykqfkowenliweltznqoiwxtqpkbkutitbohsiytamnicilkmeeywhqliyjpxygnsegyuybohrnykhfqwoztuhfbaqopgdxwbawardfnrhuldxzttmzysaunichidsphtrougfasvpfhgqduwgxkeizrllbiwawpil"))
+print(longestPalindrome("cbbd"))
+print(longestPalindrome("a"))
+print(longestPalindrome("ac"))
